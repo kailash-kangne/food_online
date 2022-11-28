b08743388dd8be765c211c088b3d41b9302294de
KAILASH KANGNE <kailash14916@gmail>
2022-11-28 20:29:07 +0530

    ok 2

--- food_site/food_site/urls.py
+++ food_site/food_site/urls.py
@@ -4,10 +4,19 @@ from django.urls import path,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.index,name='home'),
     path('accounts/', include('accounts.urls')),
+    # path('orders/', include('orders.urls')),
+    
+    #cart
+    path('cart/',MarketplaceViews.cart,name='cart'),
+    path('search/',MarketplaceViews.search,name='search'),
+    path('checkout/',MarketplaceViews.checkout,name='checkout'),
+    path('place_order/',MarketplaceViews.place_order,name='place_order'),
+    
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ food_site/marketplace/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from orders.forms import OrderForm
+
+# Create your views here.
+def cart(request):
+    return render(request, 'marketplace/cart.html')
+
+def search(request):
+    return render(request, 'marketplace/search.html')
+
+def checkout(request):
+    form = OrderForm()
+    context={
+        'form':form,
+    }
+    return render(request, 'marketplace/checkout.html',context)
+
+def place_order(request):
+    return render(request, 'marketplace/place_order.html')

d0f5f68c15d25937fb85cd509c91fe4d604af3a0
kailash-alecta <kailash.alecta@gmail.com>
2022-11-29 17:56:27 +0530

    changes done

--- food_site/accounts/forms.py
+++ food_site/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 class UserForm(forms.ModelForm):
     password=forms.CharField(widget=forms.PasswordInput())
@@ -17,4 +17,12 @@ class UserForm(forms.ModelForm):
             raise forms.ValidationError('Passwords do not match')
 
 
-
+class UserProfileForm(forms.ModelForm):
+    
+    # css style for input fields
+    profile_picture = forms.ImageField(widget = forms.FileInput(attrs={'class': 'btn btn-info'}))
+    cover_photo = forms.ImageField(widget = forms.FileInput(attrs={'class': 'btn btn-info'}))
+    
+    class Meta:
+        model=UserProfile
+        fields= ['profile_picture','cover_photo','address','country','state','city','pin_code','latitude','latitude','longitude']
--- food_site/vendor/views.py
+++ food_site/vendor/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .forms import VendorForm
+from accounts.forms  import UserProfileForm
+
+from accounts.models import UserProfile
+from .models import Vendor
+
 
 # Create your views here.
 def vprofile(request):
-    return render(request,'vendor/vprofile.html')
+    
+    profile =  get_object_or_404(UserProfile, user =request.user)
+    vendor = get_object_or_404(Vendor, user = request.user)
+    
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES , instance = profile)
+        
+            
+    profile_form = UserProfileForm(instance = profile)
+    vendor_form = VendorForm(instance = vendor)
+    
+    context={
+        'profile_form': profile_form,
+        "vendor_form": vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    
+    return render(request,'vendor/vprofile.html',context)

da4003a6f4e87a29bd8d9da68a04091215a50c01
kailash-alecta <kailash.alecta@gmail.com>
2022-12-01 18:02:13 +0530

    curd operations

--- food_site/vendor/urls.py
+++ food_site/vendor/urls.py
@@ -4,5 +4,14 @@ from accounts import views as AccountViews
 
 urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
-    path('profile/',views.vprofile,name='vprofile')
+    path('profile/',views.vprofile,name='vprofile'),
+    path('menu_builder/', views.menu_builder,name='menu_builder'),
+    path('menu_builder/category/<int:pk>', views.foodItems_by_category,name='foodItems_by_category'),
+    
+    #CURD
+    path('menu_builder/category/add/', views.add_category ,name='add_category'),
+    #path('menu_builder/category/delete/', views.delete_category ,name='delete_category'),
+    path('menu_builder/category/edit/<int:pk>', views.edit_category ,name='edit_category'),
+     
+    
 ]
